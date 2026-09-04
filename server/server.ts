fa287c7160f266d816e9e67242ac6dc2b2d09c10
waleed1857 <waleed.cool1857@gmail.com>
2026-09-04 20:35:58 +0500

    Server deployment setup

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,16 @@
 import http from "http";
 import { app } from "./app.js";
-// Dotenv configuration
 import "dotenv/config"
 import connectDB from "./config/mongodb.js";
-// import { config } from "dotenv";
-// config();
 import { initSocketServer } from "./socketServer.js";
-
 
 const server = http.createServer(app);
 initSocketServer(server);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
-// Creating server
+connectDB();
+
 server.listen(PORT, () => {
     console.log(`Server is connected on Port ${PORT}`);
-    connectDB();
 });
